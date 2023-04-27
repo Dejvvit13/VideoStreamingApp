78895b109634a0075e02bb3573ee4903c80ccf95
dejvvit13 <dawidlasek137@gmail.com>
2023-04-27 09:52:07 +0200

    Initial commit

---
+++ src/main/java/com/github/dejvvit13/videostreamingreactiveapp/VideoStreamingReactiveAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.dejvvit13.videostreamingreactiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingReactiveAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoStreamingReactiveAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/dejvvit13/videostreamingreactiveapp/VideoStreamingReactiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dejvvit13.videostreamingreactiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingReactiveAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
